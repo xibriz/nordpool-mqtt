c6351ed6a3c44e24abc95c0ef4b06f833b071466
Sten Johnsen <steoj@statoil.com>
2018-04-20 09:40:49 +0200

    Python3 compliance

--- nordpool_cache.py
+++ nordpool_cache.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 # coding: utf-8
 
-import urllib2
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+
 import json
 import datetime
-
 
 dir_path = '/path/to/nordpool-mqtt'
 # Possible values: Oslo, Kr.sand, Bergen, Tr.heim, Molde, Tromsø
 city = u'Tromsø'
 
 def save_price(date):
-	response = urllib2.urlopen('https://www.nordpoolgroup.com/api/marketdata/page/23?currency=NOK&endDate={}'.format(date))
+	response = urlopen('https://www.nordpoolgroup.com/api/marketdata/page/23?currency=NOK&endDate={}'.format(date))
 	data = json.loads(response.read())
 
 	with open('{}/cache/{}.json'.format(dir_path, date), 'w') as outfile:
--- nordpool_mqtt.py
+++ nordpool_mqtt.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 # coding: utf-8
 
-import urllib2
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+
 import json
 import datetime
 import paho.mqtt.publish as publish
